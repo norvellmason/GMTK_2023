281c712e16236d143e381339c57cb724ba86f7a3
Mason Norvell <norvellmason@gmail.com>
2023-07-06 23:45:40 -0600

    Added tests

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 1;
+        for (int i = 0; i < 10; i++)
+        {
+            x++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
